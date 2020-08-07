606c041c3c5bfc6d9f22e498b50c5b0ec3484faf
peguera-eu <peguera_eu@live.com>
2020-08-07 18:28:37 -0300

    add poison as JSON decoder | Context: need a JSON parser

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule WeatherForecast.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 
