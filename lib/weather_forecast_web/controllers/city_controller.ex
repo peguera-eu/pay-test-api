21df034b48bbbb7d2c412b90aadebba54a7ee0c6
peguera-eu <peguera_eu@live.com>
2020-08-07 18:31:17 -0300

    add controller for cities' endpoints | Context: supporting GET cities

---
+++ lib/weather_forecast_web/controllers/city_controller.ex
@@ -0,0 +1,23 @@
+defmodule WeatherForecastWeb.CityController do
+  use WeatherForecastWeb, :controller
+
+  alias WeatherForecast.Cities
+  action_fallback WeatherForecastWeb.FallbackController
+
+  def index(conn, _params) do
+    query = conn
+    |> fetch_query_params
+    fetch_cities(query)
+  end
+
+  def fetch_cities(%{query_params: %{ "top_left_lat" => top_left_lat, "top_left_lng" => top_left_lng, "bottom_right_lat" => bottom_right_lat, "bottom_right_lng" => bottom_right_lng}} = conn) do
+    cities = Cities.get_from_bounds(top_left_lat, top_left_lng, bottom_right_lat, bottom_right_lng)
+    render(conn, "index.json", cities: cities)
+  end
+
+  def fetch_cities(conn) do
+    cities = Cities.get_all()
+    render(conn, "index.json", cities: cities)
+  end
+
+end
